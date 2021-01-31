73eb454190af48a0aca03388658ff6a1351bbb9e
ybqdren <withzhaowen@126.com>
2021-01-31 21:49:21 +0800

    工厂模式

--- javagoftest/src/main/java/github/ybqdren/gof/factory/main.java
+++ javagoftest/src/main/java/github/ybqdren/gof/factory/main.java
@@ -1,8 +1,21 @@
 package github.ybqdren.gof.factory;
 
+import github.ybqdren.gof.factory.framework.Factory;
+import github.ybqdren.gof.factory.framework.Product;
+import github.ybqdren.gof.factory.idcard.IDCardFactory;
+
 /**
  * Created by Zhao Wen on 2021/1/31
  */
 
 public class main {
+    public static void main(String[] args){
+        Factory factory = new IDCardFactory();
+        Product card1 = factory.create("小明");
+        Product card2 = factory.create("小红");
+        Product card3 = factory.create("小刚");
+        card1.use();
+        card2.use();
+        card3.use();
+    }
 }
